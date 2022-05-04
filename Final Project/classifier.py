e8e2e7aa1b8ca914306eaaf43c40dc9d5409e2c2
Aiden Deffner <aidenpd100@gmail.com>
2022-05-04 09:59:10 -0700

    Completed (not ideal) classifier

--- Final Project/classifier.py
+++ Final Project/classifier.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 import nltk
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
@@ -6,6 +7,24 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('data/final_data.csv')
 pos = df[df['sentiment_class'] == 'Pos']
 neg = df[df['sentiment_class'] == 'Neg']
+train_size = int(input('Training size: '))
+pos, neg = pos[:train_size], neg[:train_size]
 
 data_list = [(row['reviews.text'], 'pos') for i, row in pos.iterrows()] + \
             [(row['reviews.text'], 'neg') for i, row in neg.iterrows()]
+
+# from DataTechNotes "Sentiment Classification with NLTK Naive Bayes Classifier"
+tokens = set(word.lower() for words in data_list for word in word_tokenize(words[0]))
+train = [({word: (word in word_tokenize(x[0])) for word in tokens}, x[1]) for x in data_list]
+
+random.shuffle(train)
+train_x, test_x = train_test_split(train, test_size=0.2)
+model = nltk.NaiveBayesClassifier.train(train_x)
+acc = nltk.classify.accuracy(model, test_x)
+print(acc)
+model.show_most_informative_features()
+
+while True:
+    test = input('Review: ')
+    t_features = {word: (word in word_tokenize(test.lower())) for word in tokens}
+    print(test," : ", model.classify(t_features))
