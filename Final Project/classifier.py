2da4683914948b8e6448088a0de451e7d6b48223
Aiden Deffner <aidenpd100@gmail.com>
2022-05-09 12:46:58 -0700

    Additional comments

--- Final Project/classifier.py
+++ Final Project/classifier.py
@@ -4,9 +4,12 @@ import nltk
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
 
+# load/read data
 df = pd.read_csv('data/final_data.csv')
 pos = df[df['sentiment_class'] == 'Pos']
 neg = df[df['sentiment_class'] == 'Neg']
+
+# input training size and split data accordingly
 train_size = int(input('Training size: '))
 pos, neg = pos[:train_size], neg[:train_size]
 
@@ -17,13 +20,17 @@ data_list = [(row['reviews.text'], 'pos') for i, row in pos.iterrows()] + \
 tokens = set(word.lower() for words in data_list for word in word_tokenize(words[0]))
 train = [({word: (word in word_tokenize(x[0])) for word in tokens}, x[1]) for x in data_list]
 
+# train model
 random.shuffle(train)
 train_x, test_x = train_test_split(train, test_size=0.2)
 model = nltk.NaiveBayesClassifier.train(train_x)
+
+# accuracy
 acc = nltk.classify.accuracy(model, test_x)
-print(acc)
+print('Accuracy:', acc)
 model.show_most_informative_features()
 
+# use model to predict review sentiment
 while True:
     test = input('Review: ')
     t_features = {word: (word in word_tokenize(test.lower())) for word in tokens}
--- Final Project/pre-trained.py
+++ Final Project/pre-trained.py
@@ -1,15 +1,18 @@
 import decimal
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+# get and return review input
 def inputReview():
     review_title = input('Write your title here: ')
     review_text = input('Write your review here: ')
     return review_title, review_text
 
+# calculate and return sentiment scores
 def getSentimentScores(review_title, review_text):
     sia = SentimentIntensityAnalyzer()
     return sia.polarity_scores(review_title), sia.polarity_scores(review_text)
 
+# scale sentiment scores to 1-5 and return rating
 def scaleScores(sentiment_scores):
     rating = 3 + 2*(sentiment_scores['compound'])
     return rating
@@ -19,6 +22,7 @@ title_score, text_score = getSentimentScores(review_title, review_text)
 title_rating, text_rating = scaleScores(title_score), scaleScores(text_score)
 avg_rating = round((title_rating + text_rating) / 2)
 
+# print prediction based on title and text
 print('Rating prediction based on title:', str(round(title_rating)))
 print('Rating prediction based on text:', str(round(text_rating)))
 print('Overall rating:', str(avg_rating))
